481cdc09d3fa16719a76a62ede3da2826dcd8428
foreignsub <c_foreigner@yahoo.com>
2019-09-10 09:53:35 +0200

    Update home.py: Adding links, rules, pairing
    List of new calls:
    del_link
    get_link
    get_links
    del_node
    set_node
    get_pairingstate (rename)
    set_rule
    get_ruleconfig
    set_ruleconfig
    get_rules
    get_secmod
    get_smsnumbers
    pairing_next
    pairing_start
    pairing_stop

--- aiofreepybox/api/home.py
+++ aiofreepybox/api/home.py
@@ -28,11 +28,41 @@ class Home:
         '''
         self._access.put('home/endpoints/{0}/'.format(nodeid), endpointid, value)
 
+    async def del_link(self, linkid):
+        '''
+        Delete link
+        '''
+        return await self._access.delete('home/links/', linkid)
+
+    async def get_link(self, linkid):
+        '''
+        Get link
+        '''
+        return await self._access.get('home/links/', linkid)
+
+    async def get_links(self):
+        '''
+        Get links
+        '''
+        return await self._access.get('home/links/')
+
+    async def del_node(self, nodeid):
+        '''
+        Delete node id
+        '''
+        return await self._access.delete('home/nodes/', nodeid)
+
     async def get_node(self, nodeid):
         '''
         Get node id
         '''
         return await self._access.get('home/nodes/', nodeid)
+
+    async def set_node(self, nodeid, nodedata):
+        '''
+        Set node data
+        '''
+        return await self._access.put('home/nodes/', nodeid, nodedata)
 
     async def get_nodes(self):
         '''
@@ -40,11 +70,47 @@ class Home:
         '''
         return await self._access.get('home/nodes/')
 
-    async def get_pairingstep(self, adapterid):
+    async def get_pairingstate(self, adapterid):
         '''
-        Get the current pairing step
+        Get the current pairing state
         '''
         return await self._access.get('home/pairing/', adapterid)
+
+    async def set_rule(self, templatename, createpayload):
+        '''
+        Set node rule
+        '''
+        return await self._access.post('home/rules/', templatename, createpayload)
+
+    async def get_ruleconfig(self, nodeid, templatename, roleid):
+        '''
+        Get node rule configuration data
+        '''
+        return await self._access.get('home/nodes/{0}/rules/template/{1}'.format(nodeid, templatename), roleid)
+
+    async def set_ruleconfig(self, rulenodeid, noderuledata):
+        '''
+        Set node rule configuration data
+        '''
+        self._access.put('home/rules/', rulenodeid, noderuledata)
+
+    async def get_rules(self, nodeid):
+        '''
+        Get node rules
+        '''
+        return await self._access.get('home/nodes/{0}/'.format(nodeid), 'rules')
+
+    async def get_secmod(self):
+        '''
+        Get security module
+        '''
+        return await self._access.get('home/secmod/')
+
+    async def get_smsnumbers(self):
+        '''
+        Get sms numbers
+        '''
+        return await self._access.get('home/sms/numbers')
 
     async def get_tile(self, tileid):
         '''
@@ -57,3 +123,21 @@ class Home:
         Get the list of Tileset
         '''
         return await self._access.get('home/tileset/all')
+
+    async def pairing_next(self, adapterid, nextpayload):
+        '''
+        Start pairing step
+        '''
+        return await self._access.post('home/pairing/', adapterid, nextpayload)
+
+    async def pairing_start(self, adapterid, startpayload):
+        '''
+        Start pairing step
+        '''
+        return await self._access.post('home/pairing/', adapterid, startpayload)
+
+    async def pairing_stop(self, adapterid, stoppayload):
+        '''
+        Stop pairing
+        '''
+        return await self._access.post('home/pairing/', adapterid, stoppayload)
