36415af750dc1fb4f2b9feeee0b4e007c341fc40
foreign-sub <c_foreigner@yahoo.com>
2019-09-24 18:20:03 +0200

    Update home.py: Add schema helpers

--- aiofreepybox/api/home.py
+++ aiofreepybox/api/home.py
@@ -4,6 +4,60 @@ class Home:
     def __init__(self, access):
         self._access = access
 
+    home_endpoint_value_schema = {
+        'value': object
+    }
+
+    create_home_node_rule_payload_schema = {
+        'iconUrl': str(),
+        'id': int,
+        'label': str(),
+        'name': str(),
+        'role': int,
+        'roleLabel': str(),
+        'type': str()
+    }
+
+    node_rule_role_schema = {
+        'node': [int],
+        'role': int
+    }
+
+    node_rule_configuration_data_schema = {
+        'roles': [node_rule_role_schema]
+    }
+
+    sms_number_data_schema = {
+        'description': str('Mon numero'),
+        'phoneNumber': str(),
+        'smsEnabled': bool(True),
+        'voicemailEnabled': bool(True)
+    }
+
+    sms_validation_data_schema = {
+        'applicationHash': str()
+    }
+
+    sms_number_validation_data_schema = {
+        'validationCode': str()
+    }
+
+    next_pairing_step_payload_schema = {
+        'session': int,
+        'pageid': int,
+        'fields': [object]
+    }
+
+    start_pairing_step_payload_schema = {
+        'nfc': bool(True),
+        'qrcode': bool(),
+        'type': str()
+    }
+
+    stop_pairing_step_payload_schema = {
+        'session': int,
+    }
+
     async def del_home_adapter(self, home_adapter_id):
         '''
         Delete home adapter
@@ -40,11 +94,11 @@ class Home:
         '''
         return await self._access.post('home/endpoints/get', endpoint_list)
 
-    async def set_home_endpoint_value(self, node_id, endpoint_id, value):
+    async def set_home_endpoint_value(self, node_id, endpoint_id, home_endpoint_value=home_endpoint_value_schema):
         '''
         Set home endpoint value
         '''
-        return await self._access.put('home/endpoints/{0}/{1}'.format(node_id, endpoint_id), value)
+        return await self._access.put('home/endpoints/{0}/{1}'.format(node_id, endpoint_id), home_endpoint_value)
 
     async def del_home_link(self, link_id):
         '''
@@ -88,11 +142,11 @@ class Home:
         '''
         return await self._access.get('home/nodes')
 
-    async def create_home_node_rule(self, template_name, create_home_node_payload):
+    async def create_home_node_rule(self, template_name, create_home_node_rule_payload=create_home_node_rule_payload_schema):
         '''
         Create home node rule
         '''
-        return await self._access.post('home/rules/{0}'.format(template_name), create_home_node_payload)
+        return await self._access.post('home/rules/{0}'.format(template_name), create_home_node_rule_payload)
 
     async def get_home_node_existing_rule_config(self, node_id, rule_node_id, role_id):
         '''
@@ -106,11 +160,11 @@ class Home:
         '''
         return await self._access.get('home/nodes/{0}/rules/template/{1}/{2}'.format(node_id, template_name, role_id))
 
-    async def set_home_node_rule_config(self, rule_node_id, node_rule_data):
+    async def set_home_node_rule_config(self, rule_node_id, node_rule_configuration_data=node_rule_configuration_data_schema):
         '''
         Set node rule configuration data
         '''
-        return await self._access.put('home/rules/{0}'.format(rule_node_id), node_rule_data)
+        return await self._access.put('home/rules/{0}'.format(rule_node_id), node_rule_configuration_data)
 
     async def get_home_node_new_rules(self, node_id):
         '''
@@ -130,7 +184,7 @@ class Home:
         '''
         return await self._access.post('home/sms/numbers', sms_number_data)
 
-    async def edit_sms_number(self, sms_number_id, sms_number_data):
+    async def edit_sms_number(self, sms_number_id, sms_number_data=sms_number_data_schema):
         '''
         Edit sms number
         '''
@@ -142,13 +196,13 @@ class Home:
         '''
         return await self._access.get('home/sms/numbers')
 
-    async def send_sms_number_validation(self, sms_number_id, sms_validation_data):
+    async def send_sms_number_validation(self, sms_number_id, sms_validation_data=sms_validation_data_schema):
         '''
         Send sms number validation
         '''
         return await self._access.post('home/sms/numbers/{0}/send_validation_sms'.format(sms_number_id), sms_validation_data)
 
-    async def validate_sms_number(self, sms_number_id, sms_number_validation_data):
+    async def validate_sms_number(self, sms_number_id, sms_number_validation_data=sms_number_validation_data_schema):
         '''
         Validate sms number
         '''
@@ -172,19 +226,19 @@ class Home:
         '''
         return await self._access.get('home/pairing/{0}'.format(home_adapter_id))
 
-    async def next_home_pairing_step(self, home_adapter_id, next_pairing_step_payload):
+    async def next_home_pairing_step(self, home_adapter_id, next_pairing_step_payload=next_pairing_step_payload_schema):
         '''
         Next home pairing step
         '''
         return await self._access.post('home/pairing/{0}'.format(home_adapter_id), next_pairing_step_payload)
 
-    async def start_home_pairing_step(self, home_adapter_id, start_pairing_step_payload):
+    async def start_home_pairing_step(self, home_adapter_id, start_pairing_step_payload=start_pairing_step_payload_schema):
         '''
         Start home pairing step
         '''
         return await self._access.post('home/pairing/{0}'.format(home_adapter_id), start_pairing_step_payload)
 
-    async def stop_home_pairing_step(self, home_adapter_id, stop_pairing_step_payload):
+    async def stop_home_pairing_step(self, home_adapter_id, stop_pairing_step_payload=stop_pairing_step_payload_schema):
         '''
         Stop home pairing
         '''
