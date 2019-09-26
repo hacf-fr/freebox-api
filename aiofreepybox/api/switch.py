1257f3ff77c967d0239432edeeded5d7b0a8b2ad
foreign-sub <c_foreigner@yahoo.com>
2019-09-26 23:48:15 +0200

    Update switch.py: update get_port_stats

--- aiofreepybox/api/switch.py
+++ aiofreepybox/api/switch.py
@@ -36,4 +36,4 @@ class Switch:
         '''
         Get port_id Port stats
         '''
-        return await self._access.get('switch/port/{}/{}'.format(port_id, 'stats'))
+        return await self._access.get('switch/port/{}/stats'.format(port_id))
