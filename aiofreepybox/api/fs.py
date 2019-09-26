88e9d5e68d0fef8625264d43e988cba19295abeb
foreign-sub <c_foreigner@yahoo.com>
2019-09-26 19:18:02 +0200

    Update fs.py: Add schema helpers

--- aiofreepybox/api/fs.py
+++ aiofreepybox/api/fs.py
@@ -13,6 +13,57 @@ class Fs:
         self._access = access
         self._path = '/'
 
+    archive_schema = {
+        'dst': str(),
+        'files': [str()]
+    }
+
+    copy_mode = [
+        'overwrite',
+        'both',
+        'recent',
+        'skip'
+    ]
+
+    copy_schema = {
+        'dst': str(),
+        'files': [str()],
+        'mode': copy_mode[0]
+    }
+
+    create_directory_schema = {
+        'dirname': str(),
+        'parent': str()
+    }
+
+    create_path_schema = {
+        'path': str(),
+    }
+
+    extract_schema = {
+        'src': str(),
+        'dst': str()
+    }
+
+    move_schema = {
+        'dst': str(),
+        'files': [str()],
+        'mode': copy_mode[0]
+    }
+
+    remove_schema = {
+        'files': [str()]
+    }
+
+    rename_schema = {
+        'src': str(),
+        'dst': str()
+    }
+
+    update_task_state_schema = {
+        'state': str()
+    }
+
     def pwd(self):
         '''
         Returns the working directory
@@ -35,7 +86,8 @@ class Fs:
         try:
             await self.get_file_info(os.path.join(self._path, path))
             return True
-        except aiofreepybox.exceptions.HttpRequestError as e:
+        except aiofreepybox.exceptions.HttpRequestError:
+            logger.debug('{} does not exist'.format(os.path.join(self._path, path)))
             return False
 
     async def ls(self):
@@ -44,25 +96,25 @@ class Fs:
         '''
         return [i['name'] for i in await self.list_file(self._path)]
 
-    async def archive_files(self, archive):
+    async def archive_files(self, archive=archive_schema):
         '''
         Archive files
         '''
         return await self._access.post('fs/archive/', archive)
 
-    async def cp(self, copy):
+    async def cp(self, copy=copy_schema):
         '''
         Copy files
         '''
         return await self._access.post('fs/copy/', copy)
 
-    async def mkdir(self, create_directory):
+    async def mkdir(self, create_directory=create_directory_schema):
         '''
         Create directory
         '''
         return await self._access.post('fs/mkdir/', create_directory)
 
-    async def mkpath(self, create_path):
+    async def mkpath(self, create_path=create_path_schema):
         '''
         Create path
         '''
@@ -74,13 +126,13 @@ class Fs:
         '''
         return await self._access.delete('fs/tasks/{}'.format(task_id))
 
-    async def rm(self, remove):
+    async def rm(self, remove=remove_schema):
         '''
         Delete files
         '''
         return await self._access.post('fs/rm/', remove)
 
-    async def extract_archive(self, extract):
+    async def extract_archive(self, extract=extract_schema):
         '''
         Extract archive
         '''
@@ -106,19 +158,19 @@ class Fs:
         path_b64 = base64.b64encode(path.encode('utf-8')).decode('utf-8')
         return await self._access.get('fs/ls/{0}'.format(path_b64))
 
-    async def mv(self, move):
+    async def mv(self, move=move_schema):
         '''
         Move files
         '''
         return await self._access.post('fs/mv/', move)
 
-    async def rename_file(self, rename):
+    async def rename_file(self, rename=rename_schema):
         '''
         Rename file
         '''
         return await self._access.post('fs/rename/', rename)
 
-    async def set_file_task_state(self, task_id, update_task_state):
+    async def set_file_task_state(self, task_id, update_task_state=update_task_state_schema):
         '''
         Set file task state
         '''
