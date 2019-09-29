dcca8c6d494e5c27ff35ebda230b36d856c2a02c
foreign-sub <c_foreigner@yahoo.com>
2019-09-29 16:56:10 +0200

    Add support for the sharelink API

---
+++ aiofreepybox/api/sharelink.py
@@ -0,0 +1,27 @@
+class Sharelink:
+
+    def __init__(self, access):
+        self._access = access
+
+    share_link_data_schema = {
+        'path': '',
+        'expire': int
+    }
+
+    async def create_share_link(self, share_link_data=share_link_data_schema):
+        '''
+        Create share link
+        '''
+        return await self._access.post('share_link/', share_link_data)
+
+    async def delete_share_link(self, token):
+        '''
+        Delete share link
+        '''
+        return await self._access.delete(f'share_link/{token}')
+
+    async def get_share_links(self):
+        '''
+        Get share links
+        '''
+        return await self._access.get('share_link/')
