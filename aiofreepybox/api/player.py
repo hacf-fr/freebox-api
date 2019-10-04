4a0795d99e36b47ecdc1fe3d78ba83b505b065d7
foreign-sub <c_foreigner@yahoo.com>
2019-10-04 23:53:26 +0200

    Update player.py: pep257

--- aiofreepybox/api/player.py
+++ aiofreepybox/api/player.py
@@ -62,37 +62,37 @@ class Player:
     }
 
     async def get_players(self):
-        '''
+        """
         Get players
-        '''
+        """
         return await self._access.get('player')
 
     async def get_player_status(self, player_id, api_version='v6'):
-        '''
+        """
         Get player status
-        '''
+        """
         return await self._access.get(f'player/{player_id}/api/{api_version}/status/')
 
     async def get_player_volume(self, player_id, api_version='v6'):
-        '''
+        """
         Get player volume
-        '''
+        """
         return await self._access.get(f'player/{player_id}/api/{api_version}/control/volume')
 
     async def set_player_volume(self, player_id, player_volume_data=player_volume_data_schema, api_version='v6'):
-        '''
+        """
         Set player volume
-        '''
+        """
         await self._access.put(f'player/{player_id}/api/{api_version}/control/volume', player_volume_data)
 
     async def open_player_url(self, player_id, player_url_data=player_url_data_schema, api_version='v6'):
-        '''
+        """
         Open player url
-        '''
+        """
         await self._access.post(f'player/{player_id}/api/{api_version}/control/open', player_url_data)
 
     async def send_media_control(self, player_id, media_control_data=media_control_data_schema, api_version='v6'):
-        '''
+        """
         Send media control
-        '''
+        """
         await self._access.post(f'player/{player_id}/api/{api_version}/control/mediactrl', media_control_data)
