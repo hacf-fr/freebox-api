9e43ddf1b7521898f6aa870edd5ca448b411b26d
foreign-sub <c_foreigner@yahoo.com>
2019-10-05 00:47:04 +0200

    Update exceptions.py: cleanup

--- aiofreepybox/exceptions.py
+++ aiofreepybox/exceptions.py
@@ -1,14 +1,3 @@
-
-class InvalidTokenError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
-
-
-class NotOpenError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
-
-
 class AuthorizationError(Exception):
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
@@ -22,3 +11,13 @@ class HttpRequestError(Exception):
 class InsufficientPermissionsError(HttpRequestError):
     def __init__(self, *args, **kwargs):
         HttpRequestError.__init__(self, *args, **kwargs)
+
+
+class InvalidTokenError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+class NotOpenError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
