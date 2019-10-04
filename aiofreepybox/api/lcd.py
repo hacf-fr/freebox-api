b7218cc34795f976d6155f3f1c907ff6e9181c7b
foreign-sub <c_foreigner@yahoo.com>
2019-10-04 23:41:44 +0200

    Update lcd.py: pep257

--- aiofreepybox/api/lcd.py
+++ aiofreepybox/api/lcd.py
@@ -10,15 +10,15 @@ class Lcd:
     }
 
     async def get_configuration(self):
-        '''
+        """
         Get configuration
-        '''
+        """
         return await self._access.get('lcd/config')
 
     async def set_configuration(self, lcd_config=None):
-        '''
+        """
         Set configuration
-        '''
+        """
         if lcd_config is None:
             lcd_config = self.lcd_config_schema
         return await self._access.put('lcd/config', lcd_config)
