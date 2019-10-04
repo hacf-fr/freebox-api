5507d1c31691aecfe418f1b5ec61733966840ad2
foreign-sub <c_foreigner@yahoo.com>
2019-10-04 23:40:50 +0200

    Updatez call.py: pep257

--- aiofreepybox/api/call.py
+++ aiofreepybox/api/call.py
@@ -11,29 +11,29 @@ class Call:
     }
 
     async def delete_call_log(self, log_id):
-        '''
+        """
         Delete call log
-        '''
+        """
         await self._access.delete(f'call/log/{log_id}')
 
     async def get_call_log(self):
-        '''
+        """
         Get call logs
-        '''
+        """
         return await self._access.get('call/log/')
 
 # TODO: remove
     async def get_call_list(self):
-        '''
+        """
         Returns the collection of all call entries
-        '''
+        """
         logger.warning('Using deprecated get_call_list, please use get_call_log instead')
         return await self.get_call_log()
 
     async def mark_call_log_as_read(self, log_id, mark_call_log_as_read_data=None):
-        '''
+        """
         Mark call log as read
-        '''
+        """
         if mark_call_log_as_read_data is None:
             mark_call_log_as_read_data = self.mark_call_log_as_read_data_schema
         return await self._access.put(f'call/log/{log_id}', mark_call_log_as_read_data)
