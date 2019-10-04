56278b99a07c9529068ae66b92326c622f8a4779
foreign-sub <c_foreigner@yahoo.com>
2019-10-04 23:44:04 +0200

    Update upload.py: pep257

--- aiofreepybox/api/upload.py
+++ aiofreepybox/api/upload.py
@@ -4,38 +4,38 @@ class Upload:
         self._access = access
 
     async def cancel_upload(self, upload_id):
-        '''
+        """
         Cancel upload
-        '''
+        """
         await self._access.delete(f'upload/{upload_id}/cancel')
 
     async def clean_uploads(self):
-        '''
+        """
         Clean uploads
-        '''
+        """
         await self._access.delete(f'upload/clean')
 
     async def delete_upload(self, upload_id):
-        '''
+        """
         Delete upload
-        '''
+        """
         await self._access.delete(f'upload/{upload_id}')
 
     async def get_uploads(self):
-        '''
+        """
         Get uploads
-        '''
+        """
         return await self._access.get('upload/')
 
     async def get_upload(self, upload_id):
-        '''
+        """
         Get upload
-        '''
+        """
         return await self._access.get(f'upload/{upload_id}')
 
 
-"""     async def upload_file(self, upload_file_start):
-        '''
+'''     async def upload_file(self, upload_file_start):
+        """
         Upload file need websocket api
-        '''
-        return await None """
+        """
+        return await None '''
