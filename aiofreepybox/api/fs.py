15bb12bffe6f1463f8587d891ccffb32884d11b0
foreign-sub <c_foreigner@yahoo.com>
2019-10-09 14:07:32 +0200

    Update fs.py: cleanup, add get_hash, hash_file

--- aiofreepybox/api/fs.py
+++ aiofreepybox/api/fs.py
@@ -45,6 +45,11 @@ class Fs:
         'dst': ''
     }
 
+    hash_file_schema = {
+        'src': '',
+        'hash_type': 'sha1'
+    }
+
     move_schema = {
         'dst': '',
         'files': [''],
@@ -98,12 +103,6 @@ class Fs:
             logger.debug('{} path does not exist'.format(os.path.join(self._path, path)))
             return False
 
-    async def ls(self):
-        """
-        List directory
-        """
-        return [i['name'] for i in await self.list_file(self._path)]
-
     async def archive_files(self, archive):
         """
         Archive files
@@ -116,48 +115,17 @@ class Fs:
         """
         return await self._access.post('fs/copy/', copy)
 
-    async def mkdir(self, create_directory=create_directory_schema):
-        """
-        Create directory
-        """
-        return await self._access.post('fs/mkdir/', create_directory)
-
-    async def mkpath(self, create_path):
-        """
-        Create path
-        """
-        return await self._access.post('fs/mkpath/', create_path)
-
     async def delete_file_task(self, task_id):
         """
         Delete file task
         """
         return await self._access.delete(f'fs/tasks/{task_id}')
 
-    async def rm(self, remove):
-        """
-        Delete files
-        """
-        return await self._access.post('fs/rm/', remove)
-
     async def extract_archive(self, extract):
         """
         Extract archive
         """
         return await self._access.post('fs/extract/', extract)
-
-    async def get_tasks_list(self):
-        """
-        Returns the collection of all tasks
-        """
-        return await self._access.get('fs/tasks/')
-
-    async def list_files(self, path, remove_hidden=0, count_sub_folder=0):
-        """
-        Returns the list of files for the given path
-        """
-        path_b64 = base64.b64encode(path.encode('utf-8')).decode('utf-8')
-        return await self._access.get(f'fs/ls/{path_b64}?removeHidden={remove_hidden}&countSubFolder={count_sub_folder}')
 
     async def get_file_info(self, path):
         """
@@ -166,17 +134,89 @@ class Fs:
         path_b64 = base64.b64encode(path.encode('utf-8')).decode('utf-8')
         return await self._access.get(f'fs/ls/{path_b64}')
 
+    async def get_hash(self, hash_id):
+        """
+        Get the hash value
+
+        To get the hash,
+        the task must have succeeded and also to be in the state “done”.
+
+        hash_id : `int`
+        """
+        return await self._access.get(f'fs/tasks/{hash_id}/hash')
+
+    async def get_tasks_list(self):
+        """
+        Returns the collection of all tasks
+        """
+        return await self._access.get('fs/tasks/')
+
+    async def hash_file(self, src, hash_type):
+        """
+        Hash a file
+
+        src : `str`
+            The file with its path
+        hash_type : `str`
+            The type of hash (md5, sha1, ...)
+        """
+        self.hash_file_schema['src'] = base64.b64encode(src.encode('utf-8')).decode('utf-8')
+        self.hash_file_schema['hash_type'] = hash_type
+        return await self._access.post('fs/hash/', self.hash_file_schema)
+
+    async def list_files(self, path, remove_hidden=0, count_sub_folder=0):
+        """
+        Returns the list of files for the given path
+        """
+        path_b64 = base64.b64encode(path.encode('utf-8')).decode('utf-8')
+        return await self._access.get(f'fs/ls/{path_b64}?removeHidden={remove_hidden}&countSubFolder={count_sub_folder}')
+
+    async def ls(self):
+        """
+        List directory
+        """
+        return [i['name'] for i in await self.list_file(self._path)]
+
+    async def mkdir(self, create_directory=create_directory_schema):
+        """
+        Create directory
+        """
+        return await self._access.post('fs/mkdir/', create_directory)
+
+    async def mkpath(self, path):
+        """
+        Create path
+
+        path : `str`
+            The path to create
+        """
+        self.create_path_schema['path'] = base64.b64encode(path.encode('utf-8')).decode('utf-8')
+        return await self._access.post('fs/mkpath/', self.create_path_schema)
+
     async def mv(self, move):
         """
         Move files
         """
         return await self._access.post('fs/mv/', move)
 
-    async def rename_file(self, rename):
+    async def rename_file(self, src, dst):
         """
         Rename file
+
+        src : `str`
+            The file with its path
+        dst : `str`
+            The new file name
         """
-        return await self._access.post('fs/rename/', rename)
+        self.rename_schema['src'] = base64.b64encode(src.encode('utf-8')).decode('utf-8')
+        self.rename_schema['dst'] = dst
+        return await self._access.post('fs/rename/', self.rename_schema)
+
+    async def rm(self, remove):
+        """
+        Delete files
+        """
+        return await self._access.post('fs/rm/', remove)
 
     async def set_file_task_state(self, task_id, update_task_state):
         """
