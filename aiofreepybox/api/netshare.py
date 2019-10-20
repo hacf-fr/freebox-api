f9d6874a72fe158d98a7e4010ddb3f2906e7adb9
foreign-sub <c_foreigner@yahoo.com>
2019-10-20 15:52:44 +0200

    Update netshare.py: docstring, format

--- aiofreepybox/api/netshare.py
+++ aiofreepybox/api/netshare.py
@@ -1,50 +1,53 @@
 class Netshare:
+    """
+    Netshare
+    """
 
     def __init__(self, access):
         self._access = access
 
     server_type = [
-        'powerbook',
-        'powermac',
-        'macmini',
-        'imac',
-        'macbook',
-        'macbookpro',
-        'macbookair',
-        'macpro',
-        'appletv',
-        'airport',
-        'xserve'
+        "powerbook",
+        "powermac",
+        "macmini",
+        "imac",
+        "macbook",
+        "macbookpro",
+        "macbookair",
+        "macpro",
+        "appletv",
+        "airport",
+        "xserve",
     ]
 
     afp_configuration_schema = {
-        'enabled': False,
-        'guest_allow': False,
-        'login_name': '',
-        'login_password': '',
-        'server_type': server_type[0]
+        "enabled": False,
+        "guest_allow": False,
+        "login_name": "",
+        "login_password": "",
+        "server_type": server_type[0],
     }
 
     samba_configuration_schema = {
-        'file_share_enabled': False,
-        'logon_enabled': False,
-        'logon_password': '',
-        'logon_user': '',
-        'print_share_enabled': True,
-        'workgroup': 'workgroup'
+        "file_share_enabled": False,
+        "logon_enabled": False,
+        "logon_password": "",
+        "logon_user": "",
+        "print_share_enabled": True,
+        "workgroup": "workgroup",
     }
 
     async def get_afp_configuration(self):
         """
         Get afp configuration
         """
-        return await self._access.get('netshare/afp/')
+        return await self._access.get("netshare/afp/")
 
     async def get_samba_configuration(self):
         """
         Get samba configuration
         """
-        return await self._access.get('netshare/samba/')
+        return await self._access.get("netshare/samba/")
 
     async def set_afp_configuration(self, afp_configuration):
         """
@@ -52,7 +55,7 @@ class Netshare:
 
         afp_configuration : `dict`
         """
-        return await self._access.put('netshare/afp/', afp_configuration)
+        return await self._access.put("netshare/afp/", afp_configuration)
 
     async def set_samba_configuration(self, samba_configuration):
         """
@@ -60,4 +63,4 @@ class Netshare:
 
         samba_configuration : `dict`
         """
-        return await self._access.put('netshare/samba/', samba_configuration)
+        return await self._access.put("netshare/samba/", samba_configuration)
