c17c889a9d0341d8d11a07c0125548239a56ba0a
foreign-sub <c_foreigner@yahoo.com>
2019-10-20 15:55:37 +0200

    Update sharelink.py: docstring, format

--- aiofreepybox/api/sharelink.py
+++ aiofreepybox/api/sharelink.py
@@ -1,12 +1,12 @@
 class Sharelink:
+    """
+    Sharelink
+    """
 
     def __init__(self, access):
         self._access = access
 
-    share_link_data_schema = {
-        'path': '',
-        'expire': 0
-    }
+    share_link_data_schema = {"path": "", "expire": 0}
 
     async def create_share_link(self, share_link_data=None):
         """
@@ -16,7 +16,7 @@ class Sharelink:
         """
         if share_link_data is None:
             share_link_data = self.share_link_data_schema
-        return await self._access.post('share_link/', share_link_data)
+        return await self._access.post("share_link/", share_link_data)
 
     async def delete_share_link(self, token):
         """
@@ -24,10 +24,10 @@ class Sharelink:
 
         token : `str`
         """
-        await self._access.delete(f'share_link/{token}')
+        await self._access.delete(f"share_link/{token}")
 
     async def get_share_links(self):
         """
         Get share links
         """
-        return await self._access.get('share_link/')
+        return await self._access.get("share_link/")
