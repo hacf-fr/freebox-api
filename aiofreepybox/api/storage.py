6087e98d4b33175b95c6e07f6eb260504ae639e6
foreign-sub <c_foreigner@yahoo.com>
2019-10-20 15:30:17 +0200

    Update storage.py: docstring, format

--- aiofreepybox/api/storage.py
+++ aiofreepybox/api/storage.py
@@ -1,11 +1,12 @@
 class Storage:
+    """
+    Storage
+    """
 
     def __init__(self, access):
         self._access = access
 
-    eject_schema = {
-        'state': 'disabled'
-    }
+    eject_schema = {"state": "disabled"}
 
     async def check_partition(self, id):
         """
@@ -13,7 +14,7 @@ class Storage:
 
         id : `int`
         """
-        return await self._access.put(f'storage/partition/{id}/check')
+        return await self._access.put(f"storage/partition/{id}/check")
 
     async def eject_disk(self, disk_id, eject_data=None):
         """
@@ -24,7 +25,7 @@ class Storage:
         """
         if eject_data is None:
             eject_data = self.eject_schema
-        return await self._access.put(f'storage/disk/{disk_id}', eject_data)
+        return await self._access.put(f"storage/disk/{disk_id}", eject_data)
 
     async def format_partition(self, id, format_data):
         """
@@ -33,13 +34,13 @@ class Storage:
         id : `int`
         format_data : `dict`
         """
-        return await self._access.put(f'storage/partition/{id}/format', format_data)
+        return await self._access.put(f"storage/partition/{id}/format", format_data)
 
     async def get_config(self):
         """
         Get storage configuration
         """
-        return await self._access.get('storage/config/')
+        return await self._access.get("storage/config/")
 
     async def get_disk(self, id):
         """
@@ -47,13 +48,13 @@ class Storage:
 
         id : `int`
         """
-        return await self._access.get(f'storage/disk/{id}')
+        return await self._access.get(f"storage/disk/{id}")
 
     async def get_disks(self):
         """
         Get disks list
         """
-        return await self._access.get('storage/disk/')
+        return await self._access.get("storage/disk/")
 
     async def get_partition(self, id):
         """
@@ -61,13 +62,13 @@ class Storage:
 
         id : `int`
         """
-        return await self._access.get(f'storage/partition/{id}')
+        return await self._access.get(f"storage/partition/{id}")
 
     async def get_partitions(self):
         """
         Get partitions list
         """
-        return await self._access.get('storage/partition/')
+        return await self._access.get("storage/partition/")
 
     async def get_raid(self, id):
         """
@@ -75,10 +76,10 @@ class Storage:
 
         id : `int`
         """
-        return await self._access.get(f'storage/raid/{id}')
+        return await self._access.get(f"storage/raid/{id}")
 
     async def get_raids(self):
         """
         Get raids list
         """
-        return await self._access.get('storage/raid/')
+        return await self._access.get("storage/raid/")
