e9f1f635e163eaffcf8d1ba5a880eec0e2a5bef7
foreign-sub <c_foreigner@yahoo.com>
2019-10-20 16:01:23 +0200

    Update rrd.py: docstring, format

--- aiofreepybox/api/rrd.py
+++ aiofreepybox/api/rrd.py
@@ -3,64 +3,45 @@ import operator
 
 
 class Rrd:
+    """
+    Rrd
+    """
 
     def __init__(self, access):
         self._setup = False
         self._access = access
 
-    db = [
-        'net',
-        'temp',
-        'dsl',
-        'switch'
-    ]
+    db = ["net", "temp", "dsl", "switch"]
 
-    fields = [
-        'time'
-    ]
+    fields = ["time"]
 
     fields_net = [
-        'bw_down',
-        'bw_up',
-        'rate_down',
-        'rate_up',
-        'vpn_rate_down',
-        'vpn_rate_up'
+        "bw_down",
+        "bw_up",
+        "rate_down",
+        "rate_up",
+        "vpn_rate_down",
+        "vpn_rate_up",
     ]
 
-    fields_fans = []
+    fields_fans = [str]
 
-    fields_temp = []
+    fields_temp = [str]
 
-    fields_temps = []
+    fields_temps = [str]
 
-    fields_dsl = [
-        'rate_down',
-        'rate_up',
-        'snr_down',
-        'snr_up'
-    ]
+    fields_dsl = ["rate_down", "rate_up", "snr_down", "snr_up"]
 
-    fields_switch_rx = [
-        'rx_1',
-        'rx_2',
-        'rx_3',
-        'rx_4'
-    ]
+    fields_switch_rx = ["rx_1", "rx_2", "rx_3", "rx_4"]
 
-    fields_switch_tx = [
-        'tx_1',
-        'tx_2',
-        'tx_3',
-        'tx_4'
-    ]
+    fields_switch_tx = ["tx_1", "tx_2", "tx_3", "tx_4"]
 
     rrd_data_schema = {
-        'dateStart': int(time.time() - 3600),
-        'dateEnd': int(time.time()),
-        'db': db[1],
-        'fields': fields_temps,
-        'precision': 10
+        "dateStart": int(time.time() - 3600),
+        "dateEnd": int(time.time()),
+        "db": db[1],
+        "fields": fields_temps,
+        "precision": 10,
     }
 
     async def init(self):
@@ -69,16 +50,16 @@ class Rrd:
             Call Init on startup to setup temp and fans fields
         """
         if not self._setup:
-            resp = await self._access.get('system/')
-            self.fields_temp = list(map(operator.itemgetter('id'), resp['sensors']))
-            self.fields_fans = list(map(operator.itemgetter('id'), resp['fans']))
+            resp = await self._access.get("system/")
+            self.fields_temp = list(map(operator.itemgetter("id"), resp["sensors"]))
+            self.fields_fans = list(map(operator.itemgetter("id"), resp["fans"]))
             self.fields_temps = self.fields_temp + self.fields_fans
             self._setup = True
 
     async def get_rrd_stats(self, rrd_data=None):
-        '''
+        """
         Get rrd stats
-        '''
+        """
         if rrd_data is None:
             rrd_data = self.rrd_data_schema
-        return await self._access.post('rrd/', rrd_data)
+        return await self._access.post("rrd/", rrd_data)
