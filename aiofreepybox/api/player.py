bc81a6a3a7d8bbc2c5c9a6be074bc904185f82e9
foreign-sub <c_foreigner@yahoo.com>
2019-10-23 01:42:27 +0200

    Update player.py: update open_player_url, set_player_volume, add update_player_volume, unlink schema

--- aiofreepybox/api/player.py
+++ aiofreepybox/api/player.py
@@ -1,3 +1,6 @@
+_DEFAULT_PLAYER_API_VERSION = "v6"
+
+
 class Player:
     """
     Player
@@ -5,21 +8,19 @@ class Player:
 
     def __init__(self, access, player_api_version=None):
         self._access = access
-        self._player_api_version_target = (
-            "v6" if player_api_version is None else player_api_version
+        self._player_api_version = (
+            _DEFAULT_PLAYER_API_VERSION
+            if player_api_version is None
+            else player_api_version
         )
 
-    player_volume_data_schema = {"mute": bool(), "volume": int}
-
-    player_url_data_schema = {"url": ""}
-
-    media_control_seek_args = {"seek_position": int, "type": "seek_position"}
+    media_control_seek_args = {"seek_position": 0, "type": "seek_position"}
 
     media_control_stream = {"quality": "", "source": ""}
 
     media_control_stream_args = {"stream": media_control_stream, "type": "stream"}
 
-    media_control_track_args = {"track_id": int, "type": "track_id"}
+    media_control_track_args = {"track_id": 0, "type": "track_id"}
 
     media_control_command = [
         "play",
@@ -57,7 +58,7 @@ class Player:
         player_id : `int`
         """
         return await self._access.get(
-            f"player/{player_id}/api/{self._player_api_version_target}/status/"
+            f"player/{player_id}/api/{self._player_api_version}/status/"
         )
 
     async def get_player_volume(self, player_id):
@@ -67,7 +68,7 @@ class Player:
         player_id : `int`
         """
         return await self._access.get(
-            f"player/{player_id}/api/{self._player_api_version_target}/control/volume"
+            f"player/{player_id}/api/{self._player_api_version}/control/volume"
         )
 
     async def get_players(self):
@@ -81,42 +82,67 @@ class Player:
         Open player url
 
         player_id : `int`
-        player_url_data : `dict`, optional
-            , by default `self.player_url_data_schema`
+        player_url_data : `str` or `dict`
+            , Default to `None`
         """
+
         if player_url_data is None:
-            player_url_data = self.player_url_data_schema
+            return
+        player_url_data_schema = dict
+        if type(player_url_data) == type({}):
+            player_url_data_schema = player_url_data
+        else:
+            player_url_data_schema["url"] = player_url_data
+
         await self._access.post(
-            f"player/{player_id}/api/{self._player_api_version_target}/control/open",
-            player_url_data,
+            f"player/{player_id}/api/{self._player_api_version}/control/open",
+            player_url_data_schema,
         )
 
-    async def send_media_control(self, player_id, media_control_data=None):
+    async def send_media_control(self, player_id, media_control_data):
         """
         Send media control
 
         player_id : `int`
-        media_control_data : `dict`, optional
-            , by default `self.media_control_data_schema`
+        media_control_data : `dict`
         """
-        if media_control_data is None:
-            media_control_data = self.media_control_data_schema
         await self._access.post(
-            f"player/{player_id}/api/{self._player_api_version_target}/control/mediactrl",
+            f"player/{player_id}/api/{self._player_api_version}/control/mediactrl",
             media_control_data,
         )
 
-    async def set_player_volume(self, player_id, player_volume_data=None):
+    async def set_player_volume(self, player_id, mute=None, volume=None):
         """
         Set player volume
 
         player_id : `int`
-        player_volume_data : `dict`, optional
-            , by default `self.player_volume_data_schema`
+        mute : `bool`, optional
+            , Default to `None`
+        volume : `int`, optional
+            , Default to `None`
         """
-        if player_volume_data is None:
-            player_volume_data = self.player_volume_data_schema
+
+        player_volume_data = dict
+        if mute is None and volume is None:
+            return
+        elif mute is not None:
+            player_volume_data["mute"] = mute
+        if volume is not None:
+            player_volume_data["volume"] = volume
+
         await self._access.put(
-            f"player/{player_id}/api/{self._player_api_version_target}/control/volume",
+            f"player/{player_id}/api/{self._player_api_version}/control/volume",
+            player_volume_data,
+        )
+
+    async def update_player_volume(self, player_id, player_volume_data):
+        """
+        Update player volume
+
+        player_id : `int`
+        player_volume_data : `dict`
+        """
+        await self._access.put(
+            f"player/{player_id}/api/{self._player_api_version}/control/volume",
             player_volume_data,
         )
