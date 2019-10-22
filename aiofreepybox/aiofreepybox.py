e159d1f0dab0ef302be165b630f6b204cd431571
foreign-sub <c_foreigner@yahoo.com>
2019-10-22 12:20:39 +0200

    Update aiofreepybox.py: None is the new auto, refactor, cleanup, format, add discover

--- aiofreepybox/aiofreepybox.py
+++ aiofreepybox/aiofreepybox.py
@@ -32,11 +32,6 @@ from aiofreepybox.api.rrd import Rrd
 from aiofreepybox.api.upnpav import Upnpav
 from aiofreepybox.api.upnpigd import Upnpigd
 
-# Token file default location
-TOKEN_FILENAME = "app_auth"
-TOKEN_DIR = os.path.dirname(os.path.abspath(__file__))
-TOKEN_FILE = os.path.join(TOKEN_DIR, TOKEN_FILENAME)
-
 # Default application descriptor
 APP_DESC = {
     "app_id": "aiofpbx",
@@ -44,6 +39,20 @@ APP_DESC = {
     "app_version": aiofreepybox.__version__,
     "device_name": socket.gethostname(),
 }
+
+# Token file default location
+TOKEN_FILENAME = "app_auth"
+TOKEN_DIR = os.path.dirname(os.path.abspath(__file__))
+TOKEN_FILE = os.path.join(TOKEN_DIR, TOKEN_FILENAME)
+
+# App defaults
+_DEFAULT_API_VERSION = "v6"
+_DEFAULT_CERT = "freebox_certificates.pem"
+_DEFAULT_HOST = "mafreebox.freebox.fr"
+_DEFAULT_HTTP_PORT = "80"
+_DEFAULT_HTTPS_PORT = "443"
+_DEFAULT_SSL = True
+_DEFAULT_TIMEOUT = 10
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -57,66 +66,33 @@ class Freepybox:
         , Default to "auto"
     """
 
-    def __init__(
-        self, app_desc=APP_DESC, token_file=TOKEN_FILE, api_version="auto", timeout=10
-    ):
-        self._access = None
-        self.api_version = api_version
-        self.api_version_target = "v6"
-        self.app_desc = app_desc
-        self.fbx_desc = {}
+    def __init__(self, app_desc=None, token_file=None, api_version=None, timeout=None):
+        self.api_version = (
+            api_version if api_version is not None else _DEFAULT_API_VERSION
+        )
+        self.app_desc = app_desc if app_desc is not None else APP_DESC
+        self.fbx_desc = None
         self.fbx_url = ""
-        self.timeout = timeout
-        self.token_file = token_file
+        self.timeout = timeout if timeout is not None else _DEFAULT_TIMEOUT
+        self.token_file = token_file if token_file is not None else TOKEN_FILE
+        self._access = None
+        self._session = None
 
-    async def open(self, host="auto", port="auto"):
+    async def open(self, host=None, port=None):
         """
         Open a session to the freebox, get a valid access module
         and instantiate freebox modules
 
         host : `str`
-            , Default to "auto"
+            , Default to `None`
         port : `str`
-            , Default to "auto"
+            , Default to `None`
         """
+
         if not self._is_app_desc_valid(self.app_desc):
             raise InvalidTokenError("Invalid application descriptor")
 
-        # Detect host, port and API version
-        default_host = host if host != "auto" else "mafreebox.freebox.fr"
-        default_port = port if port != "auto" else 80 if host != "auto" else 443
-        s = "" if default_port == 80 else "s"
-
-        # Checking host and port
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((default_host, default_port))
-        if result != 0:
-            raise HttpRequestError(
-                f"Port {default_port} is closed, cannot detect freebox"
-            )
-        sock.close()
-
-        # Session setup
-        cert_path = os.path.join(os.path.dirname(__file__), "freebox_certificates.pem")
-        ssl_ctx = ssl.create_default_context()
-        ssl_ctx.load_verify_locations(cafile=cert_path)
-        conn = aiohttp.TCPConnector(ssl_context=ssl_ctx)
-        self._session = aiohttp.ClientSession(connector=conn)
-        r = await self._session.get(
-            f"http{s}://{default_host}:{default_port}/api_version", timeout=self.timeout
-        )
-        self.fbx_desc = await r.json()
-
-        # Setting host and port
-        host = (
-            self.fbx_desc["api_domain"]
-            if host == "auto" or default_port == 80
-            else default_host
-        )
-        port = self.fbx_desc["https_port"] if port in ["auto", 80] else default_port
-        self.fbx_url = self._get_base_url(host, port)
-
-        self._check_api_version()
+        host, port = await self._check_and_validate_parameters(host, port)
 
         # Get API access
         self._access = await self._get_app_access(
@@ -149,11 +125,77 @@ class Freepybox:
         """
         Close the freebox session
         """
-        if self._access is None:
-            raise NotOpenError("Freebox is not open")
+
+        if self._access is None or self._session.closed:
+            _LOGGER.warning(f"Closing but freebox is not connected")
+            return
 
         await self._access.post("login/logout")
         await self._session.close()
+        await asyncio.sleep(0.250)
+
+    async def discover(self, default_host=None, default_port=None):
+        """
+        Discover a freebox on the network
+        """
+
+        # Setup host and port
+        if default_host is None:
+            default_host = _DEFAULT_HOST
+
+        default_port = (
+            _DEFAULT_HTTPS_PORT
+            if default_port is None and _DEFAULT_SSL
+            else default_port
+            if _DEFAULT_SSL
+            else _DEFAULT_HTTP_PORT
+        )
+        s = "" if default_port == _DEFAULT_HTTP_PORT or not _DEFAULT_SSL else "s"
+
+        # Check session
+        if (
+            self.fbx_desc is not None
+            and self._session is not None
+            and not self._session.closed
+        ):
+            conns = list(self._session._connector._conns.keys())[0]
+            if default_host != conns.host or default_port != conns.port:
+                self.fbx_desc = None
+                await self._session.close()
+                await asyncio.sleep(0.250)
+                return await self.discover(default_host, default_port)
+            else:
+                return self.fbx_desc
+
+        # Try to connect
+        if (
+            self._session is None
+            or not isinstance(self._session, aiohttp.ClientSession)
+            or self._session.closed
+        ):
+
+            # Checking host and port
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            result = sock.connect_ex((default_host, int(default_port)))
+            if result != 0:
+                return None
+            sock.close()
+
+            try:
+                cert_path = os.path.join(os.path.dirname(__file__), _DEFAULT_CERT)
+                ssl_ctx = ssl.create_default_context()
+                ssl_ctx.load_verify_locations(cafile=cert_path)
+                conn = aiohttp.TCPConnector(ssl_context=ssl_ctx)
+                self._session = aiohttp.ClientSession(connector=conn)
+            except aiohttp.client_exceptions.ClientConnectorError or aiohttp.client_exceptions.ClientConnectorCertificateError or ssl.SSLCertVerificationError:
+                return None
+
+        # Found freebox
+        r = await self._session.get(
+            f"http{s}://{default_host}:{default_port}/api_version", timeout=self.timeout
+        )
+        self.fbx_desc = await r.json()
+        return self.fbx_desc
 
     async def get_permissions(self):
         """
@@ -169,46 +211,90 @@ class Freepybox:
         until the session token is refreshed.
         If the session has not been opened yet, returns None.
         """
+
         if self._access:
             return await self._access.get_permissions()
         else:
             return None
 
-    def _check_api_version(self):
-        # Check auto and server API version
-        server_version = self.fbx_desc["api_version"].split(".")[0]
-        short_api_version_target = self.api_version_target[1:]
-        self.api_version = (
-            self.api_version_target
-            if self.api_version == "auto"
-            else f"v{server_version}"
-            if self.api_version == "server"
-            else self.api_version
+    async def _check_and_validate_parameters(self, target_host, target_port):
+        """
+        Validate host and port
+
+        target_host : `str`
+        target_port : `str`
+        """
+
+        # Discover
+        if await self.discover(target_host, target_port) is None:
+            if self._session is not None:
+                await self._session.close()
+                await asyncio.sleep(0.250)
+            raise HttpRequestError(
+                f"Cannot detect freebox on the network, please check your configuration."
+            )
+
+        # Setting host and port
+        host = (
+            self.fbx_desc["api_domain"]
+            if (
+                (target_host is None or target_port == _DEFAULT_HTTP_PORT)
+                or (target_host is not None and target_port is None)
+            )
+            and _DEFAULT_SSL
+            else _DEFAULT_HOST
+            if target_host is None and not _DEFAULT_SSL
+            else target_host
+        )
+        port = (
+            self.fbx_desc["https_port"]
+            if (target_port is None or target_port == _DEFAULT_HTTP_PORT)
+            and _DEFAULT_SSL
+            else target_port
+            if _DEFAULT_SSL
+            else _DEFAULT_HTTP_PORT
         )
 
-        # Check user api version
+        await self.discover(host, port)
+        self._check_api_version()
+        self.fbx_url = self._get_base_url(host, port)
+
+        return host, port
+
+    def _check_api_version(self):
+        """
+        Check api version
+        """
+
+        # Set API version if needed
+        server_version = self.fbx_desc["api_version"].split(".")[0]
+        short_api_version_target = _DEFAULT_API_VERSION[1:]
+        if self.api_version == "server":
+            self.api_version = f"v{server_version}"
+
+        # Check user API version
         short_api_version = self.api_version[1:]
         if (
             short_api_version_target < server_version
             and short_api_version == server_version
         ):
             _LOGGER.warning(
-                f"Using new API version {self.api_version}, results may vary "
+                f"Using new API version {self.api_version}, results may vary."
             )
         elif (
             short_api_version < short_api_version_target and int(short_api_version) > 0
         ):
             _LOGGER.warning(
-                f"Using deprecated API version {self.api_version}, results may vary "
+                f"Using deprecated API version {self.api_version}, results may vary."
             )
-        elif short_api_version > server_version:
+        elif short_api_version > server_version or int(short_api_version) < 1:
             _LOGGER.warning(
-                f"Freebox server does not support this API version ({self.api_version}), downgrading to {self.api_version_target}."
+                f"Freebox server does not support this API version ({self.api_version}), resetting to {_DEFAULT_API_VERSION}."
             )
-            self.api_version = self.api_version_target
+            self.api_version = _DEFAULT_API_VERSION
 
     async def _get_app_access(
-        self, host, port, api_version, token_file, app_desc, timeout=10
+        self, host, port, api_version, token_file, app_desc, timeout=_DEFAULT_TIMEOUT
     ):
         """
         Returns an access object used for HTTP requests.
@@ -217,12 +303,14 @@ class Freepybox:
         base_url = self._get_base_url(host, port, api_version)
 
         # Read stored application token
-        _LOGGER.info("Read application authorization file")
+        _LOGGER.debug("Reading application authorization file.")
         app_token, track_id, file_app_desc = self._readfile_app_token(token_file)
 
         # If no valid token is stored then request a token to freebox api - Only for LAN connection
         if app_token is None or file_app_desc != app_desc:
-            _LOGGER.info("No valid authorization file found")
+            _LOGGER.warning(
+                "No valid authorization file found, requesting authorization."
+            )
 
             # Get application token from the freebox
             app_token, track_id = await self._get_app_token(base_url, app_desc, timeout)
@@ -238,25 +326,25 @@ class Freepybox:
                 # denied status = authorization failed
                 if status == "denied":
                     raise AuthorizationError(
-                        "The app token is invalid or has been revoked"
+                        "The app token is invalid or has been revoked."
                     )
 
                 # Pending status : user must accept the app request on the freebox
                 elif status == "pending":
                     if not out_msg_flag:
                         out_msg_flag = True
-                        print("Please confirm the authentification on the freebox")
+                        print("Please confirm the authentification on the freebox.")
                     await asyncio.sleep(1)
 
                 # timeout = authorization failed
                 elif status == "timeout":
-                    raise AuthorizationError("Authorization timed out")
+                    raise AuthorizationError("Authorization timed out.")
 
-            _LOGGER.info("Application authorization granted")
+            _LOGGER.info("Application authorization granted.")
 
             # Store application token in file
             self._writefile_app_token(app_token, track_id, app_desc, token_file)
-            _LOGGER.info(f"Application token file was generated: {token_file}")
+            _LOGGER.info(f"Application token file was generated: {token_file}.")
 
         # Create and return freebox http access module
         fbx_access = Access(
@@ -269,6 +357,7 @@ class Freepybox:
         Get the application token from the freebox
         Returns (app_token, track_id)
         """
+
         # Get authentification token
         url = urljoin(base_url, "login/authorize/")
         data = json.dumps(app_desc)
@@ -278,7 +367,7 @@ class Freepybox:
         # raise exception if resp.success != True
         if not resp.get("success"):
             raise AuthorizationError(
-                "Authorization failed (APIResponse: {})".format(json.dumps(resp))
+                "Authorization failed (APIResponse: {}).".format(json.dumps(resp))
             )
 
         app_token = resp["result"]["app_token"]
@@ -296,6 +385,7 @@ class Freepybox:
             granted 	the app_token is valid and can be used to open a session
             denied 	    the user denied the authorization request
         """
+
         url = urljoin(base_url, f"login/authorize/{track_id}")
         r = await self._session.get(url, timeout=timeout)
         resp = await r.json()
@@ -303,23 +393,26 @@ class Freepybox:
 
     def _get_base_url(self, host, port, freebox_api_version=None):
         """
-        Returns base url for HTTPS requests
+        Returns base url for HTTP(S) requests
 
         host : `str`
         port : `str`
         freebox_api_version : `str`
             , Default to `None`
         """
+
+        s = "s" if _DEFAULT_SSL else ""
         if freebox_api_version is None:
-            return f"https://{host}:{port}"
+            return f"http{s}://{host}:{port}"
         else:
             abu = self.fbx_desc["api_base_url"]
-            return f"https://{host}:{port}{abu}{freebox_api_version}/"
+            return f"http{s}://{host}:{port}{abu}{freebox_api_version}/"
 
     def _is_app_desc_valid(self, app_desc):
         """
         Check validity of the application descriptor
         """
+
         return all(
             k in app_desc for k in ("app_id", "app_name", "app_version", "device_name")
         )
@@ -329,6 +422,7 @@ class Freepybox:
         Read the application token in the authentication file.
         Returns (app_token, track_id, app_desc)
         """
+
         try:
             with open(file, "r") as f:
                 d = json.load(f)
@@ -348,6 +442,7 @@ class Freepybox:
         """
         Store the application token in g_app_auth_file file
         """
+
         d = {**app_desc, "app_token": app_token, "track_id": track_id}
 
         with open(file, "w") as f:
