ca6f0df6112186bd115d57f0504717e46630e04a
foreign-sub <c_foreigner@yahoo.com>
2019-11-12 18:28:59 +0100

    Update rrd.py: typing, init updates and fixes

--- aiofreepybox/api/rrd.py
+++ aiofreepybox/api/rrd.py
@@ -1,5 +1,10 @@
 import time
 import operator
+from aiofreepybox.access import Access
+from typing import Dict, List, Union
+
+_UPTO_V5_FIELDS_TEMP = ["cpub", "cpum", "hdd", "sw"]
+_UPTO_V5_FIELDS_FAN = ["fan_speed"]
 
 
 class Rrd:
@@ -7,14 +12,12 @@ class Rrd:
     Rrd
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._setup = False
         self._access = access
 
     db = ["net", "temp", "dsl", "switch"]
-
     fields = ["time"]
-
     fields_net = [
         "bw_down",
         "bw_up",
@@ -23,40 +26,44 @@ class Rrd:
         "vpn_rate_down",
         "vpn_rate_up",
     ]
-
-    fields_fans = [str]
-
-    fields_temp = [str]
-
-    fields_temps = [str]
-
+    fields_fans: List[str] = []
+    fields_temp: List[str] = []
+    fields_temps: List[str] = []
     fields_dsl = ["rate_down", "rate_up", "snr_down", "snr_up"]
-
     fields_switch_rx = ["rx_1", "rx_2", "rx_3", "rx_4"]
-
     fields_switch_tx = ["tx_1", "tx_2", "tx_3", "tx_4"]
-
     rrd_data_schema = {
         "dateStart": int(time.time() - 3600),
         "dateEnd": int(time.time()),
-        "db": db[1],
-        "fields": fields_temps,
+        "db": db[0],
+        "fields": fields_net,
         "precision": 10,
     }
 
-    async def init(self):
+    async def init(self) -> None:
         """
         Init
-            Call Init on startup to setup temp and fans fields
+            Call init on startup to setup temp and fans fields
         """
+
         if not self._setup:
-            resp = await self._access.get("system/")
-            self.fields_temp = list(map(operator.itemgetter("id"), resp["sensors"]))
-            self.fields_fans = list(map(operator.itemgetter("id"), resp["fans"]))
+            a_v = self._access.base_url.split("/")
+            s_a_v = a_v[(a_v.__len__() - 2)][1:]
+
+            if int(s_a_v) > 5:
+                resp = await self._access.get("system/")
+                self.fields_temp = list(map(operator.itemgetter("id"), resp["sensors"]))
+                self.fields_fans = list(map(operator.itemgetter("id"), resp["fans"]))
+            else:
+                self.fields_temp = _UPTO_V5_FIELDS_TEMP
+                self.fields_fans = _UPTO_V5_FIELDS_FAN
+
             self.fields_temps = self.fields_temp + self.fields_fans
             self._setup = True
 
-    async def get_rrd_stats(self, rrd_data):
+    async def get_rrd_stats(
+        self, rrd_data: Dict[str, Union[int, str, List[str]]]
+    ) -> Dict[str, Union[int, List[Dict[str, int]]]]:
         """
         Get rrd stats
         """
