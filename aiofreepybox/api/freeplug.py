14583c12f5c074e6c903c3b74a301b5c81c2ec88
foreign-sub <c_foreigner@yahoo.com>
2019-12-10 20:39:29 +0100

    Update freeplug.py: typing

--- aiofreepybox/api/freeplug.py
+++ aiofreepybox/api/freeplug.py
@@ -1,18 +1,21 @@
+from typing import Any, Dict, List
+
+
 class Freeplug:
     """
     Freeplug
     """
 
-    def __init__(self, access):
+    def __init__(self, access) -> None:
         self._access = access
 
-    async def get_freeplug_networks(self):
+    async def get_freeplug_networks(self) -> List[Dict[str, Any]]:
         """
         Get freeplug networks
         """
         return await self._access.get("freeplug/")
 
-    async def reset_freeplug(self, freeplug_id):
+    async def reset_freeplug(self, freeplug_id: int) -> None:
         """
         Reset freeplug
 
