02861cd8fa373baed0b9781a4d5e6277f68fcf9a
foreign-sub <c_foreigner@yahoo.com>
2019-12-16 12:31:32 +0100

    Update fw.py: typing

--- aiofreepybox/api/fw.py
+++ aiofreepybox/api/fw.py
@@ -1,5 +1,13 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, List, Optional
+
+
 class Fw:
-    def __init__(self, access):
+    """
+    Fw
+    """
+
+    def __init__(self, access: Access) -> None:
         self._access = access
 
     ip_proto = ["tcp", "udp"]
@@ -16,7 +24,9 @@ class Fw:
     incoming_port_configuration_data_schema = {"enabled": True, "in_port": 0}
     dmz_configuration_schema = {"enabled": False, "ip": ""}
 
-    async def create_port_forwarding_configuration(self, port_forwarding_config):
+    async def create_port_forwarding_configuration(
+        self, port_forwarding_config: Dict[str, Any]
+    ):
         """
         Create port forwarding configuration
 
@@ -24,7 +34,7 @@ class Fw:
         """
         return await self._access.post("fw/redir/", port_forwarding_config)
 
-    async def delete_port_forwarding_configuration(self, config_id):
+    async def delete_port_forwarding_configuration(self, config_id: int) -> None:
         """
         Delete port forwarding configuration
 
@@ -33,7 +43,7 @@ class Fw:
         await self._access.delete(f"fw/redir/{config_id}")
 
     async def edit_incoming_port_configuration(
-        self, port_id, incoming_port_configuration_data
+        self, port_id: int, incoming_port_configuration_data: Dict[str, Any]
     ):
         """
         Edit incoming port configuration
@@ -45,30 +55,28 @@ class Fw:
             f"fw/incoming/{port_id}", incoming_port_configuration_data
         )
 
-    async def get_dmz_configuration(self):
+    async def get_dmz_configuration(self) -> Optional[Dict[str, Any]]:
         """
         Get dmz configuration
         """
         return await self._access.get("fw/dmz/")
 
-    async def get_incoming_ports_configuration(self):
+    async def get_incoming_ports_configuration(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get incoming ports configuration
         """
         return await self._access.get("fw/incoming/")
 
-    async def get_port_forwarding_configuration(self):
+    async def get_port_forwarding_configuration(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get port forwarding configuration
         """
         return await self._access.get("fw/redir/")
 
-    async def set_dmz_configuration(self, dmz_configuration=None):
+    async def set_dmz_configuration(self, dmz_configuration: Dict[str, Any]):
         """
         Set dmz configuration
 
         dmz_configuration : `dict`
         """
-        if dmz_configuration is None:
-            dmz_configuration = self.dmz_configuration_schema
         return await self._access.put("fw/dmz/", dmz_configuration)
