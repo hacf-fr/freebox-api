b769b07543629044cd22a5fe31858be0834dea8e
foreign-sub <c_foreigner@yahoo.com>
2019-12-23 18:23:42 +0100

    Update netshare.py: typing

--- aiofreepybox/api/netshare.py
+++ aiofreepybox/api/netshare.py
@@ -1,9 +1,13 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, Optional
+
+
 class Netshare:
     """
     Netshare
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access):
         self._access = access
 
     server_type = [
@@ -35,7 +39,7 @@ class Netshare:
         "workgroup": "workgroup",
     }
 
-    async def afp_switch(self, enabled=None):
+    async def afp_switch(self, enabled: bool = None) -> Optional[bool]:
         """
         Afp server switch
 
@@ -45,30 +49,32 @@ class Netshare:
         Returns `None` or enabled : `bool`
         """
 
+        config: Dict[str, Any] = {}
         if enabled is not None:
             afp_config = {"enabled": enabled}
-            config = await self.set_afp_configuration(afp_config)
+            configset = await self.set_afp_configuration(afp_config)
         else:
-            config = await self.get_afp_configuration()
-
+            configset = await self.get_afp_configuration()
+        if configset is not None:
+            config = configset
         if config["enabled"] is enabled or enabled is None:
             return config["enabled"]
         else:
             return None
 
-    async def get_afp_configuration(self):
+    async def get_afp_configuration(self) -> Optional[Dict[str, Any]]:
         """
         Get afp configuration
         """
         return await self._access.get("netshare/afp/")
 
-    async def get_samba_configuration(self):
+    async def get_samba_configuration(self) -> Optional[Dict[str, Any]]:
         """
         Get samba configuration
         """
         return await self._access.get("netshare/samba/")
 
-    async def samba_file_share_switch(self, enabled=None):
+    async def samba_file_share_switch(self, enabled: None = None) -> Optional[bool]:
         """
         Samba file share switch
 
@@ -78,18 +84,20 @@ class Netshare:
         Returns `None` or enabled : `bool`
         """
 
+        config: Dict[str, Any] = {}
         if enabled is not None:
             samba_config = {"file_share_enabled": enabled}
-            config = await self.set_samba_configuration(samba_config)
+            configset = await self.set_samba_configuration(samba_config)
         else:
-            config = await self.get_samba_configuration()
-
+            configset = await self.get_samba_configuration()
+        if configset is not None:
+            config = configset
         if config["file_share_enabled"] is enabled or enabled is None:
             return config["file_share_enabled"]
         else:
             return None
 
-    async def samba_print_share_switch(self, enabled=None):
+    async def samba_print_share_switch(self, enabled: None = None) -> Optional[bool]:
         """
         Samba print share switch
 
@@ -99,18 +107,22 @@ class Netshare:
         Returns `None` or enabled : `bool`
         """
 
+        config: Dict[str, Any] = {}
         if enabled is not None:
             samba_config = {"print_share_enabled": enabled}
-            config = await self.set_samba_configuration(samba_config)
+            configset = await self.set_samba_configuration(samba_config)
         else:
-            config = await self.get_samba_configuration()
-
+            configset = await self.get_samba_configuration()
+        if configset is not None:
+            config = configset
         if config["print_share_enabled"] is enabled or enabled is None:
             return config["print_share_enabled"]
         else:
             return None
 
-    async def set_afp_configuration(self, afp_configuration):
+    async def set_afp_configuration(
+        self, afp_configuration: Dict[str, Any]
+    ) -> Optional[Dict[str, Any]]:
         """
         Set afp configuration
 
@@ -118,7 +130,9 @@ class Netshare:
         """
         return await self._access.put("netshare/afp/", afp_configuration)
 
-    async def set_samba_configuration(self, samba_configuration):
+    async def set_samba_configuration(
+        self, samba_configuration: Dict[str, Any]
+    ) -> Optional[Dict[str, Any]]:
         """
         Set samba configuration
 
