ede5afbfbc985e140de02300d657f3d80ceb1d43
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2019-12-31 12:13:33 +0100

    Update phone.py: typing, fixes

--- aiofreepybox/api/phone.py
+++ aiofreepybox/api/phone.py
@@ -1,12 +1,16 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, List, Optional
+
+
 class Phone:
     """
     Phone
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access):
         self._access = access
 
-    async def dect_switch(self, enabled=None):
+    async def dect_switch(self, enabled: Optional[bool] = None) -> Optional[bool]:
         """
         Dect switch
 
@@ -16,36 +20,38 @@ class Phone:
         Returns `None` or enabled : `bool`
         """
 
+        config: Dict[str, Any] = {}
         if enabled is not None:
             dect_config = {"dect_enabled": enabled}
-            config = await self.set_phone_config(dect_config)
+            configset = await self.set_phone_config(dect_config)
         else:
-            config = await self.get_phone_config()
-
+            configset = await self.get_phone_config()
+        if configset is not None:
+            config = configset
         if config["dect_enabled"] is enabled or enabled is None:
             return config["dect_enabled"]
         else:
             return None
 
-    async def get_dect_vendors(self):
+    async def get_dect_vendors(self) -> Optional[List[Dict[str, str]]]:
         """
         Get dect vendors
         """
         return await self._access.get("phone/dect_vendors/")
 
-    async def get_phones(self):
+    async def get_phones(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get phones list
         """
         return await self._access.get("phone/")
 
-    async def get_phone_config(self):
+    async def get_phone_config(self) -> Optional[Dict[str, Any]]:
         """
         Get phone configuration
         """
         return await self._access.get("phone/config/")
 
-    async def set_phone_config(self, phone_entry):
+    async def set_phone_config(self, phone_entry: Dict[str, Any]):
         """
         Set phone config
 
@@ -53,7 +59,11 @@ class Phone:
         """
         return await self._access.put("phone/config/", phone_entry)
 
-    async def start_dect_configuration(self, dect_enabled=None, dect_registration=None):
+    async def start_dect_configuration(
+        self,
+        dect_enabled: Optional[bool] = None,
+        dect_registration: Optional[bool] = None,
+    ):
         """
         Start dect configuration
         To start dect configuration dect_enabled and dect_registration must be True ,
@@ -65,7 +75,7 @@ class Phone:
 
         dect_configuration = {}
         if dect_enabled is None and dect_registration is None:
-            return await get_phone_config()
+            return await self.get_phone_config()
         elif dect_enabled is not None:
             dect_configuration["dect_enabled"] = dect_enabled
         if dect_registration is not None:
