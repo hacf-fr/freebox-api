ed9142f9948292cd85d2cbdb20e0dc9b58d4256d
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2020-01-14 12:50:19 +0100

    Update storage.py: typing

--- aiofreepybox/api/storage.py
+++ aiofreepybox/api/storage.py
@@ -1,12 +1,16 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, List, Optional
+
+
 class Storage:
     """
     Storage
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._access = access
 
-    async def check_partition(self, id):
+    async def check_partition(self, id: int):
         """
         Check partition
 
@@ -14,7 +18,7 @@ class Storage:
         """
         return await self._access.put(f"storage/partition/{id}/check")
 
-    async def eject_disk(self, disk_id, eject_data=None):
+    async def eject_disk(self, disk_id: int, eject_data: Optional[Dict[str, str]]=None):
         """
         Eject storage disk
 
@@ -25,7 +29,7 @@ class Storage:
             eject_data = {"state": "disabled"}
         return await self._access.put(f"storage/disk/{disk_id}", eject_data)
 
-    async def format_partition(self, id, format_data):
+    async def format_partition(self, id: int, format_data: Dict[str, str]):
         """
         Format partition
 
@@ -34,13 +38,13 @@ class Storage:
         """
         return await self._access.put(f"storage/partition/{id}/format", format_data)
 
-    async def get_config(self):
+    async def get_config(self) -> Optional[Dict[str, Any]]:
         """
         Get storage configuration
         """
         return await self._access.get("storage/config/")
 
-    async def get_disk(self, id):
+    async def get_disk(self, id: int) -> Optional[Dict[str, Any]]:
         """
         Get disk
 
@@ -48,13 +52,13 @@ class Storage:
         """
         return await self._access.get(f"storage/disk/{id}")
 
-    async def get_disks(self):
+    async def get_disks(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get disks list
         """
         return await self._access.get("storage/disk/")
 
-    async def get_partition(self, id):
+    async def get_partition(self, id: int) -> Optional[Dict[str, Any]]:
         """
         Get partition
 
@@ -62,13 +66,13 @@ class Storage:
         """
         return await self._access.get(f"storage/partition/{id}")
 
-    async def get_partitions(self):
+    async def get_partitions(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get partitions list
         """
         return await self._access.get("storage/partition/")
 
-    async def get_raid(self, id):
+    async def get_raid(self, id: int) -> Optional[Dict[str, Any]]:
         """
         Get raid
 
@@ -76,7 +80,7 @@ class Storage:
         """
         return await self._access.get(f"storage/raid/{id}")
 
-    async def get_raids(self):
+    async def get_raids(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get raids list
         """
