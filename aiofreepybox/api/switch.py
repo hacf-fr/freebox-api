233a4d8b225dd171bc573a405ec55d9a3a0cd968
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2020-01-14 13:06:56 +0100

    Update switch.py: typing

--- aiofreepybox/api/switch.py
+++ aiofreepybox/api/switch.py
@@ -1,21 +1,25 @@
+from aiofreepybox.access import Access
+from typing import Any, Dict, List, Optional
+
+
 class Switch:
     """
     Switch
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._access = access
 
     switch_duplex = ["auto", "full", "half"]
     switch_port_configuration_schema = {"duplex": switch_duplex[0], "speed": ""}
 
-    async def get_status(self):
+    async def get_status(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get Switch status
         """
         return await self._access.get("switch/status/")
 
-    async def get_port_conf(self, port_id):
+    async def get_port_conf(self, port_id: int) -> Optional[Dict[str, Any]]:
         """
         Get port_id Port configuration
 
@@ -23,16 +27,16 @@ class Switch:
         """
         return await self._access.get(f"switch/port/{port_id}")
 
-    async def set_port_conf(self, port_id, switch_port_configuration):
+    async def set_port_conf(self, port_id: int, switch_port_configuration: Dict[str, Any]) -> Optional[Dict[str, Any]]:
         """
         Update port_id Port configuration with conf dictionary
 
         port_id : `int`
         switch_port_configuration : `dict`
         """
-        await self._access.put(f"switch/port/{port_id}", switch_port_configuration)
+        return await self._access.put(f"switch/port/{port_id}", switch_port_configuration)
 
-    async def get_port_stats(self, port_id):
+    async def get_port_stats(self, port_id: int) -> Optional[Dict[str, int]]:
         """
         Get port_id Port stats
 
