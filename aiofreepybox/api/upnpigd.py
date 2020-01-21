4c1ab57e8b49e0ee8aa1d9e39b8f990470d45b2c
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2020-01-21 11:46:32 +0100

    Update upnpigd.py: typing

--- aiofreepybox/api/upnpigd.py
+++ aiofreepybox/api/upnpigd.py
@@ -1,12 +1,17 @@
+from typing import Any, Dict, List, Optional
+
+from aiofreepybox.access import Access
+
+
 class Upnpigd:
     """
     Upnpigd
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._access = access
 
-    async def delete_redir(self, redir_id):
+    async def delete_redir(self, redir_id: int):
         """
         Deletes the given upnpigd redirection
 
@@ -14,19 +19,21 @@ class Upnpigd:
         """
         return await self._access.delete(f"upnpigd/redir/{redir_id}")
 
-    async def get_configuration(self):
+    async def get_configuration(self) -> Optional[Dict[str, Any]]:
         """
         Get the upnpigd configuration
         """
         return await self._access.get("upnpigd/config/")
 
-    async def get_redirs(self):
+    async def get_redirs(self) -> Optional[List[Dict[str, Any]]]:
         """
         Get the list of upnpigd redirections
         """
         return await self._access.get("upnpigd/redir/")
 
-    async def update_configuration(self, upnpigd_config):
+    async def update_configuration(
+        self, upnpigd_config: Dict[str, Any]
+    ) -> Optional[Dict[str, Any]]:
         """
         Update the upnpigd configuration
 
