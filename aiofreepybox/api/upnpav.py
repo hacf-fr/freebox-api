112f55e0ef77a1afe90b2ba81d01acee5ce36e71
foreign-sub <51928805+foreign-sub@users.noreply.github.com>
2020-01-21 11:38:32 +0100

    Update upnpav.py: typing

--- aiofreepybox/api/upnpav.py
+++ aiofreepybox/api/upnpav.py
@@ -1,18 +1,25 @@
+from typing import Dict, Optional
+
+from aiofreepybox.access import Access
+
+
 class Upnpav:
     """
     Upnpav
     """
 
-    def __init__(self, access):
+    def __init__(self, access: Access) -> None:
         self._access = access
 
-    async def get_configuration(self):
+    async def get_configuration(self) -> Optional[Dict[str, bool]]:
         """
         Get upnpav configuration
         """
         return await self._access.get("upnpav/config/")
 
-    async def set_configuration(self, configuration):
+    async def set_configuration(
+        self, configuration: Dict[str, bool]
+    ) -> Optional[Dict[str, bool]]:
         """
         Set upnpav configuration
 
