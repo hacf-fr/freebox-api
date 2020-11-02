1be11b56e22eb4008cce12b4b6297ca9278dd3f7
SNoof85 <snoof85@gmail.com>
2020-11-02 21:39:25 +0100

    add get and edit for a specific redir_id

--- aiofreepybox/api/fw.py
+++ aiofreepybox/api/fw.py
@@ -44,6 +44,18 @@ class Fw:
         config_id : `int`
         """
         await self._access.delete(f'fw/redir/{config_id}')
+        
+    async def get_port_forwarding(self, redir_id):	
+        '''	
+        Get a specific port forwarding	
+        '''	
+        return await self._access.get('fw/redir/{0}'.format(redir_id))	
+
+    async def edit_port_forwarding(self, redir_id, port_forwarding_config):	
+        '''	
+        Update a port forwarding	
+        '''	
+        return await self._access.put('fw/redir/{0}'.format(redir_id), port_forwarding_config)
 
     async def edit_incoming_port_configuration(self, port_id, incoming_port_configuration_data):
         """
