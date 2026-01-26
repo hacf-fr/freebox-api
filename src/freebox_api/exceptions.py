6ed717f91747fcc236247d269e80d161d0f52dc9
epenet <6771947+epenet@users.noreply.github.com>
2026-01-26 14:51:22 +0100

    Adjust

--- src/freebox_api/exceptions.py
+++ src/freebox_api/exceptions.py
@@ -1,23 +1,18 @@
 class InvalidTokenError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+    pass
 
 
 class NotOpenError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+    pass
 
 
 class AuthorizationError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+    pass
 
 
 class HttpRequestError(Exception):
-    def __init__(self, *args, **kwargs):
-        Exception.__init__(self, *args, **kwargs)
+    pass
 
 
 class InsufficientPermissionsError(HttpRequestError):
-    def __init__(self, *args, **kwargs):
-        HttpRequestError.__init__(self, *args, **kwargs)
+    pass
